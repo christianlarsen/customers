267eaa68107f69f0a51b245a4bd9c1f8ccc29b34
Christian Larsen <larsenvalverde@gmail.com>
2022-03-12 14:01:06 +0100

    Adding SQL table descriptions

--- qsqlsrc/customers.sql
+++ qsqlsrc/customers.sql
@@ -1,8 +1,23 @@
 create or replace table clv1.customers (
-    id numeric(5) not null 
-        generated always as identity 
-            (start with 7 , increment by 1),
-    descrip varchar(30) not null,
+    customer_id for             id          numeric(5) not null generated always as identity (start with 1 , increment by 1), 
+    customer_description for    descrip     varchar(30) not null,
+
     check (id >= 0),
-    primary key(id)  
+    primary key(id)
+)
+rcdfmt rcustomers;
+
+label on table clv1.customers is 'CUSTOMERS';
+
+comment on table clv1.customers is 'Any comments here...';
+
+comment on column clv1.customers (
+    id          is 'Customer ID',
+    descrip     is 'Customer description'
 );
+
+label on column clv1.customers (
+    id          is 'ID',
+    descrip     is 'Description'
+);
+
