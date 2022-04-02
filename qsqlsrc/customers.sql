00567d8985dda213b31c6bc4dd65cbee33fccfcf
Christian Larsen <larsenvalverde@gmail.com>
2022-04-02 12:18:54 +0200

    Changes

--- qsqlsrc/customers.sql
+++ qsqlsrc/customers.sql
@@ -1,6 +1,7 @@
 create or replace table clv1.customers (
     customer_id for             id          numeric(5) not null generated always as identity (start with 1 , increment by 1), 
     customer_description for    descrip     varchar(30) not null,
+    customer_address for        address     varchar(50) not null,
 
     check (id >= 0),
     primary key(id)
@@ -11,6 +12,7 @@ label on table clv1.customers is 'CUSTOMERS';
 
 label on column clv1.customers (
     id          is 'ID',
-    descrip     is 'Description'
+    descrip     is 'Description',
+    address     is 'Address'
 );
 
