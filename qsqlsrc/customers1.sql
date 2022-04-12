5228d8f17dff7724ce30a0e95b66e1044bb001a1
Christian Larsen <larsenvalverde@gmail.com>
2022-04-12 17:03:25 +0200

    Changing Customers view

--- qsqlsrc/customers1.sql
+++ qsqlsrc/customers1.sql
@@ -9,5 +9,12 @@ create or replace view clv1.customers1 as
     from clv1.customers c
     exception join clv1.orders o on
         c.id = o.customerid 
-    group by c.id, c.descrip
+    group by c.id, c.descrip;
 
+label on table clv1.customers1 is 'CUSTOMERS View (1)';
+
+label on column clv1.customers1 (
+    id          is 'ID',
+    descrip     is 'Description',
+    orders      is 'Nr of Orders'
+);
